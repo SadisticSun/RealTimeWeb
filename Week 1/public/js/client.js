77da2cfbf23c59c199dc1d33cb4c0505763b1fe0
SadisticSun <tristan@iamt.nl>
2017-04-12 23:47:42 +0200

    Feature(s) added: Chat sounds, online user list

--- Week 1/public/js/client.js
+++ Week 1/public/js/client.js
@@ -1,24 +1,57 @@
 
 (function(){
+
+// Socket Declaration
 var socket = io();
 
+// DOM elements
 var $loginArea = document.getElementById('login-area');
 var $loginForm = document.getElementById('login-form');
 var $username = document.getElementById('username');
+var $userList = document.getElementById('user-list');
 var $chatArea = document.getElementById('chat');
 var $chatForm = document.getElementById('chat-form');
 var $message = document.getElementById('message');
 var $chat = document.getElementById('messages');
+var $chatSound = document.getElementById('audio');
 
+// Set an empty user variable for later use
 var user;
 
+// Declare windowIsActive variable
+var windowIsActive = false;
+
+function playSound(){
+  /*
+      Play sound when window is inactive
+      NOTE: Due to browser resource usage restrictions,
+      sounds will not be played on the first time the browser is inactive
+   */
+
+    if (windowIsActive) {
+    $chatSound.innerHTML = '<audio id="audio" src="/public/sounds/chat.mp3" autoplay></audio>';
+  }
+}
+
+// Set windowIsActive to true when windows is in focus
+window.onblur = function(){
+    windowIsActive = true;
+};
+// Set windowIsActive to false when window is out of focus or minimized
+window.onfocus = function(){
+    windowIsActive = false;
+};
+
+// Login form handler
 $loginForm.addEventListener('submit', function(event) {
   event.preventDefault();
 
+// Send new user information to server
   socket.emit('new user', $username.value, function(name) {
       socket.emit('setUsername', name);
   });
 
+// Show main window when username is available and succesfully sent to server
   socket.on('userSet', function(data){
     user = data.username;
     console.log(data.username + ' has logged in.');
@@ -26,26 +59,39 @@ $loginForm.addEventListener('submit', function(event) {
     $chatArea.classList.remove('hidden');
   });
 
+// If the username exists, show error
   socket.on('userExists', function(data){
     document.getElementById('error-container').innerHTML = data;
   });
 
+// Empty the input field
   $username.value = '';
 
 });
 
+// Chat message form handler
 $chatForm.addEventListener('submit', function(event) {
   event.preventDefault();
   socket.emit('send message', {message: $message.value, user: user});
   $message.value = '';
 });
 
+// Update online user list
+socket.on('update USERS', function(data) {
+  var html = '';
+  data.forEach(function(onlineUser){
+    console.log('Online: ' + onlineUser);
+    html += '<li class="user-list">' + onlineUser + '</li>';
+    $userList.innerHTML = html;
+  });
+});
+
+// New message handler
 socket.on('new message', function(data) {
     console.log(data.user + ' says: ' + data.message);
     $chat.innerHTML += '<li><b>' + data.user + ':</b>' + ' ' + data.message;
+    playSound();
     window.scrollTo(0, document.body.scrollHeight);
  });
-
-return false;
 
 })();
--- Week 1/server.js
+++ Week 1/server.js
@@ -7,13 +7,13 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-var users = [];
+var USERS = [];
 var connections = [];
 
 app.use('/public', express.static('./public'));
 
 // Server setup
-server.listen(process.env.PORT || 3000);
+server.listen(3000);
 console.log('[Server] Running on: http://localhost:3000');
 
 // Routes
@@ -21,33 +21,34 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+
 // Socket.io
 io.on('connection', function(socket) {
   connections.push(socket);
 
   // Username set
   socket.on('setUsername', function(name){
-    if (users.indexOf(name) > -1 ) {
-        console.log(users);
+    if (USERS.indexOf(name) > -1 ) {
+        console.log(USERS);
         socket.emit('userExists', name + ' is taken! Try another username.');
     } else {
-      users.push(name);
+      USERS.push(name);
       console.log(name);
-      console.log(users);
+      console.log(USERS);
       socket.emit('userSet', {username: name});
+      io.sockets.emit('update USERS', USERS);
     }
   });
 
-  socket.emit('welcome', { message: 'Welcome to the chat.' });
   console.log('[Server] Connected: %s user(s) connected', connections.length);
 
   // Disconnect
   socket.on('disconnect', function(){
-    if (!socket.username) return;
-    users.splice(users.indexOf(socket.username), 1);
-
+    USERS.splice(USERS.indexOf(socket.username), 1);
     connections.splice(connections.indexOf(socket), 1);
     console.log('[Server] Disconnected: %s user(s) still connected', connections.length);
+    console.log(USERS);
+    io.sockets.emit('update USERS', USERS);
   });
 
   // Send message
@@ -59,6 +60,9 @@ io.on('connection', function(socket) {
   socket.on('new user', function(data, callback) {
     callback(data);
     socket.username = data;
-
+    console.log(USERS);
+    io.sockets.emit('update USERS', USERS);
   });
+
+
 });
