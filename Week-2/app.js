91aba5202de5004bb370171bc4594a12adbd636d
SadisticSun <tristan@iamt.nl>
2017-04-21 16:17:18 +0200

    Working OAuth connection with YouTube

--- Week-2/app.js
+++ Week-2/app.js
@@ -1,36 +1,85 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var app = express();
-var index = require('./routes/index');
-var server = require('http').createServer(app);
 
-// listen to port 3000
-server.listen(3000);
-console.log('[Server] Running on: http://localhost:3000');
+/*jshint esversion: 6 */
 
-// view engine setup
+// Dependencies
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const request = require('request');
+const server = require('http').createServer(app);
+
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// Set up .env support
+const dotenv = require('dotenv').config();
 
+// Get .env variables
+const api_key             = process.env.API_KEY;
+const client_id           = process.env.CLIENT_ID;
+const client_secret       = process.env.CLIENT_SECRET;
+const response_type       = process.env.RESPONSE_TYPE;
+const grant_type          = process.env.GRANT_TYPE;
+const scope               = process.env.SCOPE;
+const redirect_uri        = process.env.REDIRECT_URI;
 
-// error handler
-app.use(function(err, req, res, next) {
+const google_apis          = 'https://www.googleapis.com/';
 
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Authorization Code Request URL
+var request_url =
+'https://accounts.google.com/o/oauth2/auth?client_id='+ client_id + '&redirect_uri=' + redirect_uri + '&scope=' + scope + '&response_type=' + response_type + '&access_type=offline';
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// ROUTES
+/* GET home page. */
+app.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'OAUTH API test',
+    request_url: request_url
+  });
 });
+
+/* GET login page. */
+app.get('/login', function(req, res) {
+  var response_code = req.query.code;
+
+  // Access Token POST request URL
+  var post_url =
+  google_apis + 'oauth2/v4/token?code='+ response_code + '&client_id='+ client_id + '&client_secret=' + client_secret + '&redirect_uri=' + redirect_uri + '&grant_type=' + grant_type;
+
+  // Set empty variables
+  var TOKEN;
+  var REFRESH_TOKEN;
+  var EXPIRATION_DATE;
+
+  // Do POST request to Google API
+  request.post(post_url, function (error, response, body) {
+    // Parse response string into JSON
+    parsed_data = JSON.parse(body);
+
+    // Assign properties of the returned JSON to variables for later use
+    TOKEN             = parsed_data.access_token;
+    REFRESH_TOKEN     = parsed_data.refresh_token;
+    EXPIRATION_DATE   = parsed_data.expires_in;
+
+  });
+
+  request(google_apis + 'youtube/v3/channels?access_token=' + TOKEN + '&part=snippet&mine=true', function(error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(body);
+    }
+  });
+  res.render('login');
+});
+
+// Listen to port 3000
+server.listen(3000);
+console.log('[Server] Running on: http://localhost:3000');
 
 module.exports = app;
